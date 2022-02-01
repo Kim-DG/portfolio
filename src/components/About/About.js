154f980375904435eb7333bb06ff466075b3647b
KDG3536 <81103357+Kim-DG@users.noreply.github.com>
2022-02-02 01:21:45 +0900

    contact, about me title 크기 수정

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <div className="about_me" id="about">
       <div>
-        <h1 className="about_me_title">프론트 앤드 개발자 - 김대경</h1>
+        <h1 className="about_me_title">프론트 엔드 개발자 - 김대경</h1>
         <section className="about_me_section">
           <p>
             새로운 도전을 좋아하는 개발자입니다. <br /> 실행력과 의지, 도전적인
