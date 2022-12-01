e2ebc838f20094fa4b913ca2cf8207233ddef34b
Kim-DG <kdg5746@gmail.com>
2022-12-01 18:43:35 +0900

    이전버전 업데이트

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,11 +5,12 @@ function About() {
   return (
     <div className="about_me" id="about">
       <div>
-        <h1 className="about_me_title">프론트 엔드 개발자 - 김대경</h1>
+        <h1 className="about_me_title">안드로이드 개발자 - 김대경</h1>
         <section className="about_me_section">
           <p>
-            새로운 도전을 좋아하는 개발자입니다. <br /> 실행력과 의지, 도전적인
-            정신을 가지고 있습니다.
+            새로운 도전을 좋아하는 개발자입니다. <br /> 도전적인
+            정신과 의지, 
+            실행력을 가지고 있습니다.
           </p>
         </section>
       </div>
